adefce4ace5954e236d1db796679d66fa1b610a0
Shravan Kumar Talabhaktula <shravan@Shravans-MacBook-Pro.local>
2025-10-14 20:51:54 -0400

    initial commit, added basic routes

---
+++ src/components/Home.component.jsx
@@ -0,0 +1,10 @@
+function HomeComponent() {
+    return (
+        <>
+            <h1>Welcome!</h1>
+            <p>This is hackerrank reactjs programs</p>
+        </>
+    )
+}
+
+export default HomeComponent
---
+++ src/components/ItemListManager.component.jsx
@@ -0,0 +1,9 @@
+function ItemListManagerComponent() {
+    return (
+        <>
+            <h1>Item List Manager</h1>
+        </>
+    )
+}
+
+export default ItemListManagerComponent;
